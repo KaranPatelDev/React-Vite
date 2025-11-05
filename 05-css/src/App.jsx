eed1adc82f47f409051d70c8cc03773464027c6e
Karantechie <karankpguinfo001018@gmail.com>
2025-11-05 22:34:34 +0530

    05-css completed

---
+++ 05-css/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './components/Button/Button.module'
+import Header from './components/Header/Header.module'
+
+const App = () => {
+  return (
+    <div>
+      <Button />
+      <Header />
+    </div>
+  )
+}
+
+export default App
