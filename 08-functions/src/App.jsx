3a8868e17bf0e254677c2f0fdcd32f8260efd4dd
Karantechie <karankpguinfo001018@gmail.com>
2025-11-07 23:46:41 +0530

    08 -  updated

--- 08-functions/src/App.jsx
+++ 08-functions/src/App.jsx
@@ -1,37 +1,93 @@
 import React from 'react'
 
+// const App = () => {
+
+//   const btnClicked = () => {
+//     console.log("Button Clicked")
+//   }
+
+//   function mouseEnter(){
+//     console.log("Mouse Entered")
+//   }
+
+//   function inputChanging(){
+//     console.log("User is typing!!")
+//   }
+
+//   return (
+//     <div>
+//       <button onMouseEnter={mouseEnter} onClick={btnClicked}>Click Here</button>
+//       <button onMouseEnter={mouseEnter} onClick={btnClicked}>Explore This</button>
+//       <input onChange={inputChanging} type="text" placeholder='Enter Your Name' />
+
+//       <input onChange={function(){
+//         console.log("User is typing!!")
+//       }} type="text" placeholder='Enter Your Name : ' />
+
+
+//       <input onChange={function(elem){
+//         console.log(elem)
+//         console.log(elem.target.value)
+//       }} type="text" placeholder='Enter Your Name : ' />
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+
+
+
+
+
+// const App = () => {
+//   return (
+//     <div>
+//       <div onClick={(elem) => {
+//         console.log("Red Light You are dead !!!!!")
+//         console.log(elem.clientX, elem.clientY)
+//       }} className="box"></div>
+//     </div>
+//   )
+// }
+
+// export default App;
+
+
+
+
+
+
+
+
+
+
 const App = () => {
 
-  const btnClicked = () => {
-    console.log("Button Clicked")
-  }
+  const pageScrolling = (elem) => {
+    if(elem > 0){
+      console.log("Scrolling Down - Seedha Scroll")
+    }else{
+      console.log("Scrolling Up - Ulta Scroll")
+    }
+}
 
-  function mouseEnter(){
-    console.log("Mouse Entered")
-  }
-
-  function inputChanging(){
-    console.log("User is typing!!")
-  }
 
   return (
-    <div>
-      {/* <h1>Karan Patel</h1> */}
-      <button onMouseEnter={mouseEnter} onClick={btnClicked}>Click Here</button>
-      <button onMouseEnter={mouseEnter} onClick={btnClicked}>Explore This</button>
-      <input onChange={inputChanging} type="text" placeholder='Enter Your Name' />
-
-      <input onChange={function(){
-        console.log("User is typing!!")
-      }} type="text" placeholder='Enter Your Name : ' />
-
-
-      <input onChange={function(elem){
-        console.log(elem)
-        console.log(elem.target.value)
-      }} type="text" placeholder='Enter Your Name : ' />
+    <div onWheel={(elem) => {
+      console.log(elem)
+      console.log(elem.deltaX)
+      console.log(elem.deltaY)
+      console.log(elem.deltaZ)
+      pageScrolling(elem)
+    }}>
+      <div className="page1"></div>
+      <div className="page2"></div>
+      <div className="page3"></div>
     </div>
   )
 }
 
-export default App
+export default App;
